63d0c38661d827614fb0236e15a3a63d94509d25
Роман <125956341+moduleb@users.noreply.github.com>
2024-08-29 00:31:08 +0400

    исправил строки подключения

--- bot_app/config.py
+++ bot_app/config.py
@@ -19,6 +19,7 @@ def load_env_or_default(variable_name, default):
 WEBSOCKET_HOST = load_env_or_default('WEBSOCKET_HOST', 'localhost')
 WEBSOCKET_CONNECTION_STRING = f'ws://{WEBSOCKET_HOST}:8000/ws'
 
+# УБРАТЬ ПОСЛЕ ПЕРЕНОСА В ФАСТАПИ!!!!!!
 """ GRPC """
 GRPC_HOST = load_env_or_default('GRPC_HOST', 'localhost')
 GRPC_CONNECTION_STRING = f'{GRPC_HOST}:50051'
